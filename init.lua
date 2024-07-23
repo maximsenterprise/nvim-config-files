486f2f407876a9cac9abdb647cebd353499ccedd
Maxims Enterprise <maximsenterprise@gmail.com>
2024-07-23 17:18:50 +0200

    Added flexibility for non-block commentable languages

--- init.lua
+++ init.lua
@@ -15,12 +15,12 @@ local function insert_sign()
 	-- Define el texto que queremos insertar, usando una tabla para múltiples líneas
 	local lines = {
 		"",
-		string.format("	%s", file_name),
-		string.format("	As part of the %s project", current_folder),
-		string.format("	Created by Maxims Enterprise in %s", current_year),
-		"	--------------------------------------------------",
-		string.format("	Description: %s", vim.fn.input("Description: ")),
-		string.format("	Copyright (c) %s Maxims Enterprise", current_year),
+		string.format(" %s", file_name),
+		string.format(" As part of the %s project", current_folder),
+		string.format(" Created by Maxims Enterprise in %s", current_year),
+		" --------------------------------------------------",
+		string.format(" Description: %s", vim.fn.input("Description: ")),
+		string.format(" Copyright (c) %s Maxims Enterprise", current_year),
 	}
 
 	if vim.bo.filetype == "python" then
@@ -37,7 +37,17 @@ local function insert_sign()
 	-- Mueve el cursor a la línea recién insertada
 	vim.api.nvim_cmd({ cmd = "normal", args = { "gg" } }, {})
 	vim.api.nvim_cmd({ cmd = "normal", args = { "V" .. #lines .. "j" } }, {})
-	vim.api.nvim_cmd({ cmd = "normal", args = { "gb" } }, {})
+
+	local not_block_langauges = {
+		"asm",
+		"make",
+		"ld",
+	}
+	if vim.tbl_contains(not_block_langauges, vim.bo.filetype) then
+		vim.api.nvim_cmd({ cmd = "normal", args = { "gc" } }, {})
+	else
+		vim.api.nvim_cmd({ cmd = "normal", args = { "gb" } }, {})
+	end
 end
 
 -- Define el comando `:sign` que llama a la función `insert_sign`

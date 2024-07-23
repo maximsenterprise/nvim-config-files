d0344d8f74574ff33cbfa758de78f6c1185f103e
Maxims Enterprise <maximsenterprise@gmail.com>
2024-07-23 17:10:41 +0200

    Better support for my custom Sign function

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 require("maxvdec.core.init")
 require("maxvdec.lazy")
-
 -- Agrega esto al principio de tu init.lua
 
 local function insert_sign()
@@ -15,17 +14,30 @@ local function insert_sign()
 
 	-- Define el texto que queremos insertar, usando una tabla para múltiples líneas
 	local lines = {
-		string.format("%s", file_name),
-		string.format("As part of the %s project", current_folder),
-		string.format("Created by Maxims Enterprise in %s", current_year),
 		"",
+		string.format("	%s", file_name),
+		string.format("	As part of the %s project", current_folder),
+		string.format("	Created by Maxims Enterprise in %s", current_year),
+		"	--------------------------------------------------",
+		string.format("	Description: %s", vim.fn.input("Description: ")),
+		string.format("	Copyright (c) %s Maxims Enterprise", current_year),
 	}
+
+	if vim.bo.filetype == "python" then
+		table.insert(lines, 1, '"""')
+		table.insert(lines, '"""')
+	end
 
 	-- Inserta las líneas en una nueva línea al principio del archivo
 	vim.api.nvim_buf_set_lines(0, 0, 0, false, lines)
 
+	if vim.bo.filetype == "python" then
+		return
+	end
 	-- Mueve el cursor a la línea recién insertada
-	vim.api.nvim_cmd({ cmd = "normal", args = { "gg", "V2j", "gc" } }, {})
+	vim.api.nvim_cmd({ cmd = "normal", args = { "gg" } }, {})
+	vim.api.nvim_cmd({ cmd = "normal", args = { "V" .. #lines .. "j" } }, {})
+	vim.api.nvim_cmd({ cmd = "normal", args = { "gb" } }, {})
 end
 
 -- Define el comando `:sign` que llama a la función `insert_sign`
